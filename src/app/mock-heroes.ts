0c331f479eaead7ffddf8b60081d58e51a1d4781
Fedde Tilma <feddetilma@ip-192-168-2-8.eu-west-1.compute.internal>
2021-02-16 11:07:02 +0100

    Heroes component

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Lux' },
+  { id: 12, name: 'Neeko' },
+  { id: 13, name: 'Mundo' },
+  { id: 14, name: 'Nasus' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
